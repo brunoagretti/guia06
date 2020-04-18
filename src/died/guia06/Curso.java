89cbb97423b76a4971164a8243f38a10913b750a
brunoagretti <brunoagretti@hotmail.com>
2020-04-18 17:25:41 -0300

    resuelvo paso 01

--- src/died/guia06/Curso.java
+++ src/died/guia06/Curso.java
@@ -1,5 +1,6 @@
 package died.guia06;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,16 +47,25 @@ public class Curso {
 	 * @return
 	 */
 	public Boolean inscribir(Alumno a) {
-		log.registrar(this, "inscribir ",a.toString());
-		return false;
+		try {
+			log.registrar(this, "inscribir ",a.toString());
+			return true;
+		} catch (IOException e1) {
+			System.out.println("Ha ocurrido un error: " + e1);
+			return false;
+		}
 	}
 	
 	
-	/**
+	/** 
 	 * imprime los inscriptos en orden alfabetico
 	 */
 	public void imprimirInscriptos() {
-		log.registrar(this, "imprimir listado",this.inscriptos.size()+ " registros ");
+		try {
+			log.registrar(this, "imprimir listado",this.inscriptos.size()+ " registros ");
+		} catch (IOException e2) {
+			System.out.println("Ha ocurrido un error: " + e2);
+		}
 	}
 
 
